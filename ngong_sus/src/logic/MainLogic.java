18810d876f1bac97470575ecccf5ef41c0cebe1a
Ammari Jetthakun <ammarinjtk@outlook.com>
2015-12-09 00:28:44 +0700

    all about main logic ver.1

--- ngong_sus/src/logic/MainLogic.java
+++ ngong_sus/src/logic/MainLogic.java
@@ -1,6 +1,54 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class MainLogic {
+	public static int boxZ;
+	public int creationDelay;
+	public int creationDelayCounter;
+	private ArrayList<Box> boxes;
+	private Knight knight;
+	private Enemy enemy;
+	private Bar bar;
+	private boolean isPause;
 	
+	public MainLogic(){
+		boxes = new ArrayList<Box>();
+		knight = new Knight(1, 5, 50);
+		enemy = new Enemy(2, 7, 50);
+		bar = new Bar();
+		setPause(false);
+		creationDelay = 5;
+		creationDelayCounter = 0;
+		
+	}
 	
+	public void generateBox(){
+		
+	}
+
+	public boolean isPause() {
+		return isPause;
+	}
+
+	public void setPause(boolean isPause) {
+		this.isPause = isPause;
+	}
+
+	public ArrayList<Box> getBoxes() {
+		return boxes;
+	}
+
+	public Knight getKnight() {
+		return knight;
+	}
+
+	public Enemy getEnemy() {
+		return enemy;
+	}
+
+	public Bar getBar() {
+		return bar;
+	}
+
 }
