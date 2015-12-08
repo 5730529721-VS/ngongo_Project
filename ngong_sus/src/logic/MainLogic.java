ad075667e1f8141326856397b4a39f68b315ec53
Ammari Jetthakun <ammarinjtk@outlook.com>
2015-12-09 01:00:46 +0700

    Add generator

--- ngong_sus/src/logic/MainLogic.java
+++ ngong_sus/src/logic/MainLogic.java
@@ -2,6 +2,8 @@ package logic;
 
 import java.util.ArrayList;
 
+import render.RenderableHolder;
+
 public class MainLogic {
 	public static int boxZ;
 	public int creationDelay;
@@ -23,8 +25,24 @@ public class MainLogic {
 		
 	}
 	
-	public void generateBox(){
+	public void start(){
 		
+	}
+	
+	public void generateBox(){
+		double rand = Math.random()*4;
+		if (rand > 0 && rand < 1){
+			RenderableHolder.getInstance().add(new RedBox((int)(Math.random()*10), (int)(Math.random()*5)));
+		}
+		else if (rand < 2){
+			RenderableHolder.getInstance().add(new GreenBox((int)(Math.random()*20)));
+		}
+		else if (rand < 3){
+			RenderableHolder.getInstance().add(new PurpleBox((int)(Math.random()*10)));
+		}
+		else {
+			RenderableHolder.getInstance().add(new YellowBox((int)(Math.random()*30)));
+		}
 	}
 
 	public boolean isPause() {
